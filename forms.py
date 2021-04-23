fd011fa46063b826d9a8661b83a0993b1d2883db
Zackmoody <71839279+Zackmoody@users.noreply.github.com>
2021-04-23 13:01:03 -0400

    forms.py for update_items

--- forms.py
+++ forms.py
@@ -6,5 +6,4 @@ class ItemsForm(ModelForm):
     class Meta:
         model = Items
         fields = ['Items_ID', 'Items_Name', 'Items_Stock', 'Items_Sale_Price', 'Items_Department'] #These attribute names are different in your models.py file 
-                                                                                                   #you will need to change them to 'items_id' for example
-                                                                                                   
+                                                                                                   #you will need to change them to 'items_id' for example                                                                                          
