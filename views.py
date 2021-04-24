e84ac58cb762cc33f0bcf267777f3148959a3c6f
mdawe17 <72044442+mdawe17@users.noreply.github.com>
2021-04-24 13:13:34 -0400

    Update views.py

--- views.py
+++ views.py
@@ -37,3 +37,43 @@ def updateItems(request, pk):
     context = {'form' : form}
     return render(request,'update_items.html', context)
 
+def search(request):
+    return render(request, 'storeinventorydb/search.html')
+
+def search_results(request):
+    searchName = request.GET['name']
+
+    query_set_name_filtered = []
+    query_set_name = Items.objects.all()
+
+    for object in query_set_name:
+        if (searchName in object.items_name):
+            query_set_name_filtered.append(object)
+    context = {
+        'object_list' : query_set_name_filtered
+    }
+
+    return render(request, 'storeinventorydb/search_results.html', context)
+
+
+
+def createItem(request):
+    form = NewItem()
+    if request.method == "POST":
+        form = NewItem(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('')
+            
+    context = {'form' : form}
+    return render(request, 'storeinventorydb/cni.html', context)
+
+
+def deleteOrder(request, pk):
+	item = Items.objects.get(items_id=pk)
+	if request.method == "POST":
+		item.delete()
+		return redirect('/home')
+
+	context = {'item':item}
+	return render(request, 'storeinventorydb/delete.html', context)
