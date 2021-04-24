7f384907d838cc0f7e9b87eaf07258dc7fe8d2b9
mdawe17 <72044442+mdawe17@users.noreply.github.com>
2021-04-24 13:10:09 -0400

    Update forms.py

--- forms.py
+++ forms.py
@@ -2,8 +2,16 @@ from django.forms import ModelForm
 from .models import *
 
 
+
 class ItemsForm(ModelForm):
     class Meta:
         model = Items
-        fields = ['Items_ID', 'Items_Name', 'Items_Stock', 'Items_Sale_Price', 'Items_Department'] #These attribute names are different in your models.py file 
-                                                                                                   #you will need to change them to 'items_id' for example                                                                                          
+        fields = ['items_id', 'items_name', 'items_stock', 'items_sale_price', 'items_dept'] 
+        #These attribute names are different in your models.py file 
+        #you will need to change them to 'items_id' for example   
+
+
+class NewItem(ModelForm): 
+    class Meta:
+        model = Items
+        fields = "__all__"  
